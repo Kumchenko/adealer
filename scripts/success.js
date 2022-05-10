6a8262ebe3035d62ee64a0793b7c21f0d676f91a
Kumchenko <kirillkumchenko@gmail.com>
2022-05-10 22:40:40 +0300

    Return to (Action)

--- scripts/success.js
+++ scripts/success.js
@@ -1,9 +1,2 @@
-const select = document.getElementById('select-model');
-select.addEventListener("change", changeModel);
-
-
-function changeModel() {
-    const modelImage = document.getElementById('form-model-image');
-    var url = "url('./assets/images/iphones/" + this.value + ".png')";
-    modelImage.style.backgroundImage = url;
-}
+const returnButton = document.getElementById('return');
+returnButton.addEventListener("click", function(){click_button("./order.html")} );
